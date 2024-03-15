35ca06d36885f9055a115c83a9ad712bb902e85d
Rajveer Yadav <51945519+ray-27@users.noreply.github.com>
2024-03-16 04:49:07 +0530

    cuda

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
+import torch
+
+if torch.cuda.is_available():
+    DEVICE = "cuda"
+elif torch.backends.mps.is_available():
+    DEVICE = "mps"
+else: DEVICE = "cpu"
+print(f'device model is running on : {DEVICE}')
 
 
 print("yeah this is test")
