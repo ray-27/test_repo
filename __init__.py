b50750c01a000e2505f127c7a9041e201a8f5820
Rajveer Yadav <51945519+ray-27@users.noreply.github.com>
2024-03-29 00:01:35 +0530

    sd

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 
-from . import fun
+__all__ = ["fun","drive","test"]
--- test.py
+++ test.py
@@ -1,16 +1,10 @@
 
-# if torch.cuda.is_available():
-#     DEVICE = "cuda"
-# elif torch.backends.mps.is_available():
-#     DEVICE = "mps"
-# else: DEVICE = "cpu"
-# print(f'device model is running on : {DEVICE}')
 
-import test_repo.fun as f
+import fun as f
 
 def train(a):
 
     print("yeah this is test")
     f.pri(a)
 
-train(5)
+train(2)
